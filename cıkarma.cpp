76839996ccc423db88efb96599570b7e0d983828
Nisanur Özer <nnozer34@gmail.com>
2025-10-30 11:44:56 +0300

    Add files via upload
    16008125050 
    sevgi merve doğan

---
+++ cıkarma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cout << "Birinci sayiyi gir: ";
+    cin >> a;
+    cout << "ikinci sayiyi gir: ";
+    cin >> b;
+    double sonuc = a - b;
+    cout << a << " - " << b << " = " << sonuc << endl;
+    return 0;
+}
